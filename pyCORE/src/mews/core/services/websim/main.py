ecede797a12652c210ac9615327126539be8f61e
Brian Ricks <bwr031000@utdallas.edu>
2018-02-20 23:55:15 -0600

    initial commit of python for webcrawler

---
+++ pyCORE/src/mews/core/services/websim/main.py
@@ -0,0 +1,12 @@
+'''
+Created on Jan 19, 2018
+
+@author: Brian Ricks
+'''
+import mews.core.services.websim.sitecrawler
+
+if __name__ == '__main__':
+    crawler = mews.core.services.websim.sitecrawler.SiteCrawler();
+    
+    crawler.setsite("https://www.cmu.edu/silicon-valley")
+    crawler.site_crawl()
