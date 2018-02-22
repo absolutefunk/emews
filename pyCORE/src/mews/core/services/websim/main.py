f9d67c7bf5bd0416856acf06d2115c9b6314b93f
Brian Ricks <bwr031000@utdallas.edu>
2018-02-22 00:38:09 -0600

    linted added prefix list for bad links to avoid

--- pyCORE/src/mews/core/services/websim/main.py
+++ pyCORE/src/mews/core/services/websim/main.py
@@ -6,7 +6,7 @@ Created on Jan 19, 2018
 import mews.core.services.websim.sitecrawler
 
 if __name__ == '__main__':
-    crawler = mews.core.services.websim.sitecrawler.SiteCrawler();
+    crawler = mews.core.services.websim.sitecrawler.SiteCrawler()
 
-    crawler.setsite("https://www.wellsfargo.com")
+    crawler.setsite("https://www.cmu.edu/silicon-valley")
     crawler.site_crawl()
--- pyCORE/src/mews/core/services/websim/sitecrawler.py
+++ pyCORE/src/mews/core/services/websim/sitecrawler.py
@@ -3,20 +3,24 @@ Created on Jan 19, 2018
 
 @author: Brian Ricks
 '''
-import math
-import mechanize
-import random
-import scipy.stats
-import time
 
+import random
+import time
+import scipy.stats
+import mechanize
 
 class SiteCrawler(object):
     '''
     classdocs
     '''
-    _DEFAULT_PAGE_COUNT = 5  # number of sequential links to follow when crawling
-    _DEFAULT_LINK_STDDEV = 0.8  # std deviation of picking a link around mu (from list of links)
-    _DEFAULT_PATH_LINK_STDDEV = 0.2  # std deviation of picking a link around mu in a path of links (after first iteration)
+    # number of sequential links to follow when crawling
+    _DEFAULT_PAGE_COUNT = 5
+    # std deviation of picking a link around mu (from list of links)
+    _DEFAULT_LINK_STDDEV = 0.8
+    # std deviation of picking a link around mu in a path of links (after first iteration)
+    _DEFAULT_PATH_LINK_STDDEV = 0.2
+    # prefix list for links which we should NOT visited
+    _INVALID_LINK_PREFIX = ["java", "none"]
 
     def __init__(self):
         '''
@@ -31,15 +35,28 @@ class SiteCrawler(object):
         self.init_config()
 
     def init_config(self):
+        '''
+        init configuration params
+        '''
+
         # no, I am not a robot ;-)
         self._br.set_handle_robots(False)
         # set user agent string to something real-world
-        self._br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0')]
+        self._br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; Ubuntu; Linux \
+        x86_64; rv:57.0) Gecko/20100101 Firefox/57.0')]
 
     def setsite(self, sitename):
+        '''
+        sets the URL to start crawling from
+        '''
+
         self._siteURL = sitename
 
-    def truncnorm_link_index(self, num_links, itr = None):
+    def truncnorm_link_index(self, num_links, itr=None):
+        '''
+        selects the link index to visit next
+        '''
+
         mu = (num_links - 1) / 2.0  # pick the middle link to distribute around
 
         if itr is not None and itr > 0:
@@ -50,25 +67,35 @@ class SiteCrawler(object):
         lower = 0  # lower bound
         upper = num_links - 1  # upper bound
 
-        dist = scipy.stats.truncnorm((lower - mu) / sigma, (upper - mu) / sigma, loc=mu, scale=sigma)
+        dist = scipy.stats.truncnorm(
+            (lower - mu) / sigma, (upper - mu) / sigma, loc=mu, scale=sigma)
 
         return int(round(dist.rvs(1)[0]))
 
     def checklink(self, link_str):
-        # TODO: generalize this as a list
-        
-        if link_str.absolute_url.lower().startswith("java"):
-            return False
+        '''
+        checks a link object's URL for validity (not a javascript link or something)
+        that we can't visit
+        '''
+
+        for prefix in self._INVALID_LINK_PREFIX:
+            if link_str.absolute_url.lower().startswith(prefix):
+                return False
 
         return True
 
     def site_crawl(self):
+        '''
+        crawls a web site, starting from the _siteURL, picking links from
+        each page visited
+        '''
         self._br.open(self._siteURL)
 
         for itr in range(self._num_links_to_follow):
             page_links = list(self._br.links())
 
-            if len(page_links) == 0:
+            # ie, if page_link length is zero
+            if not page_links:
                 print "[SiteCrawler]: Reached page without any links, stopping..."
                 return
 
@@ -76,23 +103,25 @@ class SiteCrawler(object):
 
             while len(page_links) > 1:
                 # keep looping until a valid link is found
-                selected_link_index = self.truncnorm_link_index(len(page_links), itr = itr)
+                selected_link_index = self.truncnorm_link_index(len(page_links), itr=itr)
 
-                #print page_links[selected_link_index].base_url
+                # print page_links[selected_link_index].base_url
                 if self.checklink(page_links[selected_link_index]):
                     break
 
                 # strip the bad link from the list and try again
                 del page_links[selected_link_index]
 
-            if len(page_links) == 0:
+            if not page_links:
                 print "[SiteCrawler]: Reached page with links, but none were valid, stopping..."
                 return
 
             next_link = page_links[selected_link_index]
 
             # introduce a delay to simulate user moving mouse and clicking link
-            time.sleep(random.randint(1,8))
+            time.sleep(random.randint(1, 8))
 
-            print "[" + str(selected_link_index) + "/" + str(len(page_links)) + "]: " + next_link.absolute_url
-            self._br.follow_link(link = next_link)
+            print "[" + str(selected_link_index) + "/" + str(len(page_links)) + "]: " \
+            + next_link.absolute_url
+
+            self._br.follow_link(link=next_link)
