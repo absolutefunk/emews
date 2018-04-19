10a2d6ffc680f965f77b59472c417f0dc2a1b6ea
Brian Ricks <bwr031000@utdallas.edu>
2018-04-19 02:40:54 -0500

    - added comment to version.py stating that the file is used to determine project root.

--- pyCORE/src/emews/version.py
+++ pyCORE/src/emews/version.py
@@ -1,4 +1,7 @@
 '''
 emews versioning info
+
+Note: this file is used as a reference to obtain the <root> path for the emews project directory
+structure.  It must recide in the root project (emews) directory.
 '''
 __version__ = "0.3"
