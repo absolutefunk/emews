b4a7024b4af3ff327fa573b89cfcdc52a7b2a372
Brian Ricks <bwr031000@utdallas.edu>
2018-04-23 04:24:43 -0500

    SpawningListener --> MultiListener No longer inherited, composition instead. Callback to handle spawned sockets, thread_dispatcher no longer part of listener.

--- pyCORE/src/emews/base/connectionmanager.py
+++ pyCORE/src/emews/base/connectionmanager.py
@@ -7,11 +7,12 @@ Created on Mar 24, 2018
 '''
 import signal
 
+import emews.base.baseobject
 import emews.base.clientsession
-import emews.base.spawninglistener
+import emews.base.multilistener
 import emews.base.thread_dispatcher
 
-class ConnectionManager(emews.base.spawninglistener.SpawningListener):
+class ConnectionManager(emews.base.baseobject.BaseObject):
     '''
     classdocs
     '''
@@ -25,6 +26,8 @@ class ConnectionManager(emews.base.spawninglistener.SpawningListener):
         signal.signal(signal.SIGHUP, self.shutdown_signal_handler)
         signal.signal(signal.SIGINT, self.shutdown_signal_handler)
 
+        # listener
+        self._listener = emews.base.multilistener.MultiListener(self.config, self.handle_connection)
         # handles thread spawning/management
         self._thread_dispatcher = emews.base.thread_dispatcher.ThreadDispatcher(self.config)
 
@@ -33,10 +36,22 @@ class ConnectionManager(emews.base.spawninglistener.SpawningListener):
         Signal handler for incoming signals (those which may imply we need to shutdown)
         '''
         self.logger.info("Received signum %d, beginning shutdown...", signum)
-        self.stop()  # shut down the listener
+        self._listener.stop()  # shut down the listener
 
-    def update_spawnclass_args(self):
+    def start(self):
         '''
-        @Override return the ThreadDispatcher, as the ClientSession needs it
+        Starts the ConnectionManager.
         '''
-        return [self.dispatcher]
+        try:
+            self._listener.start()
+        except StandardError as ex:
+            self.logger.error("Listener failed: %s", ex)
+
+        self._thread_dispatcher.shutdown_all_threads()  # stop all dispatched threads
+
+    def handle_connection(self, sock):
+        '''
+        @Override start a ClientSession with this socket
+        '''
+        self._thread_dispatcher.dispatch_thread(
+            emews.base.clientsession.ClientSession(self.config, sock, self._thread_dispatcher))
---
+++ pyCORE/src/emews/base/multilistener.py
@@ -0,0 +1,70 @@
+'''
+Multi-connection listener, in which each accepted connection is passed off to a handler method.
+
+When requested to stop, we shutdown the socket, which unblocks select.
+
+Created on Apr 22, 2018
+@author: Brian Ricks
+'''
+import os
+import select
+import socket
+
+import emews.base.baselistener
+
+class MultiListener(emews.base.baselistener.BaseListener):
+    '''
+    classdocs
+    '''
+    def __init__(self, config, connection_handler_cb):
+        '''
+        Constructor
+        '''
+        super(MultiListener, self).__init__(config)
+        # callback for providing handling of sockets spawned
+        self._connection_handler = connection_handler_cb
+
+        self._interrupted = False  # sets to true when stop() invoked
+
+    @property
+    def interrupted(self):
+        '''
+        returns whether the listener has been requested to stop
+        '''
+        return self._interrupted
+
+    def listen(self):
+        '''
+        @Override starts the listening loop for connections
+        '''
+        while not self._interrupted:
+            try:
+                select.select([self.socket], [], [])
+            except select.error as ex:
+                # interrupted (most likely)
+                if self.interrupted:
+                    self.logger.info("Listener no longer accepting incoming connections.")
+                    break
+                else:
+                    self.logger.error("Select error while blocking on lister socket.")
+                    raise StandardError(ex)
+
+            try:
+                sock, src_addr = self.socket.accept()
+            except socket.error as ex:
+                # Just ignore this exception, as emews can keep running
+                self.logger.warning("Exception when accepting incoming connection: %s", ex)
+                continue
+
+            self.logger.info("Connection established from %s", src_addr)
+            sock.setblocking(0)
+            self._connection_handler(sock)   # call connection handler
+
+        os.close(self._int_write_fd)  # close the pipe
+
+    def stop(self):
+        '''
+        @Override Invoked when it is time to shut down.
+        '''
+        self._interrupted = True
+        self.socket.shutdown()
