464410dc3e19058c2a8d5e1b157a30f5099f4324
Brian Ricks <absolutefunk@gmail.com>
2018-08-30 14:22:42 -0500

    - TODO: test eMews when run with python -OO (removes docstrings / asserts) - Standardzie docstrings!

--- pyCORE/src/emews/base/config.py
+++ pyCORE/src/emews/base/config.py
@@ -5,11 +5,10 @@ Created on Apr 3, 2018
 
 @author: Brian Ricks
 '''
+import textwrap
+
 from ruamel.yaml import YAML
 
-'''
-Module-level functions related to configuration loading/parsing.
-'''
 def parse(filename):
     '''
     Parses the given filename (if it exists), and returns a dictionary.
@@ -23,3 +22,23 @@ def parse(filename):
     dct = yaml.load(f)
 
     return dct
+
+def make_config_cls(name, fields):
+    '''
+    Creates a class type for a configuration class.  Each eMews class which requires configuration
+    will have its own type associated with it.
+    Adapted from:
+    stackoverflow.com/questions/2646157/what-is-the-fastest-to-access-struct-like-object-in-python
+    except __getitem__ is removed as index based access is not required and we can potentially
+    save some memory.
+    '''
+    conf_cls = textwrap.dedent("""\
+    class {name}(object):
+        __slots__ = {fields!r}
+        def __init__(self, {args}):
+            {self_fields} = {args}
+    """).format(name=name, fields=fields, args=','.join(fields),
+                self_fields=','.join('self.' + f for f in fields))
+    exec_dict = {'fields': fields}
+    exec conf_cls in exec_dict  # pylint: disable=W0122
+    return exec_dict[name]
