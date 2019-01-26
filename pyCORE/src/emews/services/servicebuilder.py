5445f1d00a4f86d103302b06be1cb51685acb7b5
Brian Ricks <absolutefunk@gmail.com>
2019-01-25 20:07:57 -0600

    ServiceBuilder: finished updating _build_modifiers(), and removed _instantiate_dependencies()

--- pyCORE/src/emews/services/servicebuilder.py
+++ pyCORE/src/emews/services/servicebuilder.py
@@ -45,7 +45,8 @@ class ServiceBuilder(emews.base.baseobject.BaseObject):
 
         # instantiate service object
         try:
-            service_obj = service_class(service_config['config'], _inject=service_config['inject'])
+            service_obj = service_class(service_config['parameters'],
+                                        _inject=service_config['inject'])
             cls.logger.debug("Service class '%s' instantiated.", service_name)
         except StandardError:
             cls.logger.error("Service '%s' could not be instantiated.", service_name)
@@ -90,93 +91,31 @@ class ServiceBuilder(emews.base.baseobject.BaseObject):
                 current_instantiation = emews.base.import_tools.import_class(
                     module_name, class_name)
             except ImportError:
-                cls.logger.error("Modifier module '%s' could not be imported.",
-                                 modifier_name.lower())
+                cls.logger.error("Modifier module '%s' for service '%s' could not be imported.",
+                                 modifier_name.lower(), service_obj.__class__.__name__)
                 raise
 
             try:
-                current_instantiation = current_instantiation(_inject=base_decorator_config)
-            except AttributeError as ex:
-                self.logger.error("Extension '%s' could not be instantiated: %s",
-                                  decorator_name, ex)
+                current_instantiation = current_instantiation(modifier_config['parameters'],
+                                                              _inject=modifier_config['inject'])
+            except StandardError:
+                cls.logger.error("Modifier '%s' for service '%s' could not be instantiated.",
+                                 modifier_name, service_obj.__class__.__name__)
                 raise
 
-            self.logger.debug("Extension '%s' applied to %s.", decorator_name, service_obj.__class__.__name__)
+            cls.logger.debug("Modifier '%s' applied to service '%s'.",
+                             modifier_name, service_obj.__class__.__name__)
             prev_instantiation = current_instantiation
 
         return current_instantiation
 
-    def _instantiate_dependencies(self, config):
-        '''
-        Instantiates (or at least imports the class) all dependency objects declared in the input
-        config.
-        '''
-        dependency_instantiation_dict = dict()
-        for dep_name, dependency in config.iteritems():
-            self.logger.debug("Resolving helper '%s'.", dep_name)
-            try:
-                module_name, class_name = self._get_path_and_class(
-                    'emews.helpers', dependency['helper'])
-            except KeyError as ex:
-                self.logger.error("Required key missing from configuration for helper '%s': %s",
-                                  dep_name, ex)
-                raise
-
-            # check optional params
-            b_instantiate = True
-            if 'instantiate' in dependency:
-                if not isinstance(dependency['instantiate'], bool):
-                    self.logger.error("In helper '%s': Key 'instantiate' must have a "\
-                                      "boolean value (given value: %s).",
-                                      dep_name, dependency['instantiate'])
-                    raise ValueError(
-                        "Key 'instantiate' must have a boolean value (given value: %s)." %
-                        str(dependency['instantiate']))
-                else:
-                    b_instantiate = dependency['instantiate']
-
-            try:
-                class_object = emews.base.importclass.import_class_from_module(
-                    module_name, class_name)
-            except ImportError as ex:
-                self.logger.error("Could not import helper '%s': %s",
-                                  dep_name, ex)
-                raise
-
-            # Currently dependencies are not allowed to have dependencies.
-            if 'config' in dependency:
-                dep_config = dependency['config']
-                self.logger.debug("Found config information for helper '%s'.", dep_name)
-            else:
-                dep_config = {}
-                self.logger.debug("No config information found for helper '%s'.", dep_name)
-
-            emews.base.config.Config(dep_config)
-
-            if b_instantiate:
-                try:
-                    dependency_instantiation_dict[dep_name] = class_object(
-                        _inject={'config': dep_config})
-                except AttributeError as ex:
-                    self.logger.error("Helper '%s' could not be instantiated: %s", dep_name, ex)
-                    raise
-                self.logger.debug("Helper '%s' instantiated.", dep_name)
-            else:
-                # instantiation not requested
-                # DelayedInstantiation is callable.  self.helpers.<dep_name>.instantiate() to
-                # instantiate.
-                dependency_instantiation_dict[dep_name] = \
-                    {'instantiate': \
-                        emews.base.config.DelayedInstantiation(class_object, dep_config)}
-                self.logger.debug("Helper '%s' requested not to instantiate.", dep_name)
-
-        return dependency_instantiation_dict
-
     @classmethod
     def _process_config(cls, config):
-        '''
-        Process the raw config dict to a ServiceConfig.
-        '''
+        """
+        Format input config dict to a raw dict suitable for passing to services.
+
+        Include injection key for K/Vs which need to be injected into a base class.
+        """
         base_service_config = dict()
         base_service_config['config'] = {} if config is None else config.get('config', {})
         if config is not None and 'helpers' in config:
