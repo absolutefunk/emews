5b219d93f594785ed132c006e54afcd69cf50f90
Brian Ricks <absolutefunk@gmail.com>
2019-02-20 14:14:05 -0600

    - formatting updates on ConnectionManager

--- pyCORE/src/emews/base/connectionmanager.py
+++ pyCORE/src/emews/base/connectionmanager.py
@@ -1,21 +1,19 @@
-'''
+"""
 Listens for new client connections and spawns ClientSessions when appropriate.
 
 Created on Mar 24, 2018
 @author: Brian Ricks
-'''
+"""
 import emews.base.clientsession
 import emews.base.ihandlerlistener
 import emews.base.multilistener
 
+
 class ConnectionManager(emews.base.ihandlerlistener.IHandlerListener):
-    '''
-    classdocs
-    '''
+    """Classdocs."""
+
     def __init__(self, config, thread_dispatcher):
-        '''
-        Constructor
-        '''
+        """Constructor."""
         super(ConnectionManager, self).__init__()
 
         self._listener = emews.base.multilistener.MultiListener(config, self)
@@ -24,27 +22,19 @@ class ConnectionManager(emews.base.ihandlerlistener.IHandlerListener):
         self._thread_dispatcher = thread_dispatcher
 
     def start(self):
-        '''
-        Starts the ConnectionManager.
-        '''
+        """Start the ConnectionManager."""
         self._listener.start()  # start the listener
 
     def stop(self):
-        '''
-        Stops the ConnectionManager
-        '''
+        """Stop the ConnectionManager."""
         self._listener.stop()  # shut down the listener
 
     def handle_accepted_connection(self, sock):
-        '''
-        @Override start a ClientSession with this socket
-        '''
+        """@Override start a ClientSession with this socket."""
         self._thread_dispatcher.dispatch_thread(
             emews.base.clientsession.ClientSession(self._config, sock, self._thread_dispatcher),
             force_start=True)
 
     def handle_readable_socket(self, sock):
-        '''
-        @Override not implemented due to listener not using this callback
-        '''
+        """@Override not implemented due to listener not using this callback."""
         pass
